1cee7ba736bc8f04064a7e60ba1b2e33f2b37ca8
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-07-16 20:14:08 -0300

    LightHouse Light Rotation

---
+++ Assets/Scripts/Ambient/LightHouseRotation.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightHouseRotation : MonoBehaviour {
+
+    [SerializeField] GameObject lightHouseLight;
+    [SerializeField] float speed = 0.1f;
+
+    void Update() {
+        lightHouseLight.transform.Rotate(0.0f, speed, 0.0f, Space.World);
+    }
+}
