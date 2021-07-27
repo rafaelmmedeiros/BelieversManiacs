45ccae89e3de639f0bf1da8bddc04adaadcfb1ec
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-07-26 21:40:16 -0300

    Sounds Triggers

---
+++ Assets/Scripts/OneShotSound.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneShotSound : MonoBehaviour {
+
+    [SerializeField] bool oneTime = false;
+    [SerializeField] float pauseTime = 5.0f;
+
+    private AudioSource audioSource;
+    private new Collider collider;
+
+    void Start() {
+
+        audioSource = GetComponent<AudioSource>();
+        collider = GetComponent<Collider>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+
+        if (other.CompareTag("Player")) {
+            audioSource.Play();
+            collider.enabled = false;
+
+            if (oneTime == false) {
+                StartCoroutine(Reset());
+            } else {
+                Destroy(gameObject, pauseTime);
+            }
+        }
+    }
+
+    IEnumerator Reset() {
+        yield return new WaitForSeconds(pauseTime);
+        collider.enabled = true;
+    }
+}
