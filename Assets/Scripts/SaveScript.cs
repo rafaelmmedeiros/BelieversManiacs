5dd35b711d256a1596bfd368cf1000ae19e12123
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-07-27 00:05:21 -0300

    UI Elements

---
+++ Assets/Scripts/SaveScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveScript : MonoBehaviour {
+
+    public static int playerHealth = 85;
+    public static bool healthChanged = false;
+
+}
