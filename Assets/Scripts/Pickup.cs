8a42cbea7bda8b2b55c2f909bf92656f89df0a6c
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-07-28 20:22:45 -0300

    Pickup

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+    RaycastHit raycastHit;
+    [SerializeField] float distance;
+    [SerializeField] GameObject pickupMessage;
+
+    private float raycastDistance;
+    private bool pickupInRange;
+
+    void Start() {
+
+        pickupInRange = false;
+        pickupMessage.gameObject.SetActive(false);
+        raycastDistance = distance;
+    }
+
+    void Update() {
+
+        if (Physics.Raycast(transform.position, transform.forward, out raycastHit, raycastDistance)) {
+
+            if (raycastHit.transform.tag == "Apple") {
+                pickupInRange = true;
+            } else {
+                pickupInRange = false;
+            }
+        }
+
+        if (pickupInRange == true) {
+            pickupMessage.gameObject.SetActive(true);
+            raycastDistance = 1000f;
+        }
+
+        if (pickupInRange == false) {
+            pickupMessage.gameObject.SetActive(false);
+            raycastDistance = distance;
+        }
+
+    }
+}
